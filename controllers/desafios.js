51092c107d9f66d7292fa0809252e6ec8f5d6efc
Caique Alves <caiquealves224@gmail.com>
2019-03-20 23:38:54 -0300

    refatorando para separar controler da model

--- controllers/desafios.js
+++ controllers/desafios.js
@@ -1,28 +1,4 @@
-const db = require("../db");
-const connection = db.mysql;
-
-function buscar(){
-
-  return new Promise(function(resolve,reject){
-    connection.query("SELECT a.* from challengers a inner join users_challengers up on (up.challenger_id = a.id)  inner join users u on (u.id = up.users_id);", function(err,rows){
-      if (err) reject(err);
-      else resolve(rows);
-    });
-  });
-}
-
-function buscarPorUsuario(req){
-
-  return new Promise(function(resolve,reject){
-    connection.query(`
-    SELECT a.* from challengers a
-     inner join users_challengers up on (up.challenger_id = a.id) 
-     inner join users u on (u.id = up.users_id) where u.id = ${req.params.id};`, function(err,rows){
-      if (err) reject(err);
-      else resolve(rows);
-    });
-  });
-}
+const usuarios = require("./model/usuario");
 
 const desafios = function(req,res){
   buscar()
---
+++ models/usuario.js
@@ -0,0 +1,33 @@
+const db = require("../db");
+const connection = db.mysql; 
+
+function index(){
+
+  return new Promise(function(resolve,reject){
+    connection.query(`
+      SELECT a.* from challengers a 
+      inner join users_challengers up on (up.challenger_id = a.id) 
+      inner join users u on (u.id = up.users_id);`, function(err,rows){
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+function find(id){
+
+  return new Promise(function(resolve,reject){
+    connection.query(`
+      SELECT a.* from challengers a
+      inner join users_challengers up on (up.challenger_id = a.id) 
+      inner join users u on (u.id = up.users_id) where u.id = ${id};`, function(err,rows){
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+module.exports = {
+  index,
+  find
+}
